3622680869f9441ef38bf7664796fae4b8fc3ebe
hishamrehab <hishamrehab30@gmail.com>
2026-01-21 13:53:53 +0200

    Componnet Compostion

--- 01 - React Essentials - Components, JSX, Props, State & More/react-essentials/src/App.jsx
+++ 01 - React Essentials - Components, JSX, Props, State & More/react-essentials/src/App.jsx
@@ -1,34 +1,46 @@
-import reatImg from './assets/react-core-concepts.png';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-
-function Header() {
-   const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-   <header>
-        <img src={reatImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  )
-}
-
+import { CORE_CONCEPTS } from './data';
+import Header from './components/header/Header';
+import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
         <h2>Time to get started!</h2>
+          <CoreConcept 
+          title={CORE_CONCEPTS[0].title}
+          description={CORE_CONCEPTS[0].description}
+          image={CORE_CONCEPTS[0].image}
+          />
+        
+         <CoreConcept 
+           {...CORE_CONCEPTS[0]}
+          />   
+         
+          <CoreConcept 
+           {...CORE_CONCEPTS[1]}
+          />   
+
+           <CoreConcept 
+           {...CORE_CONCEPTS[2]}
+          />   
+
+           <CoreConcept 
+           {...CORE_CONCEPTS[3]}
+          />   
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+              <TabButton label="Hooks" />
+              <TabButton label="Components" />
+              <TabButton label="Context" />
+              <TabButton label="Redux" />
+          </menu>
+        </section>
       </main>
     </div>
   );
---
+++ 01 - React Essentials - Components, JSX, Props, State & More/react-essentials/src/components/TabButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TabButton = ({label}) => {
+  return (
+    <li>
+        <button>
+            {label}
+        </button>
+    </li>
+  )
+}
+
+export default TabButton
