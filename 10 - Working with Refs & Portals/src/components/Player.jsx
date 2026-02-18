f26181cdbcf77c2e43dd4358b551292307fea315
hishamrehab <hishamrehab30@gmail.com>
2026-02-19 00:01:21 +0200

    Introducing Refs and working with HTML Elements

--- 10 - Working with Refs & Portals/src/components/Player.jsx
+++ 10 - Working with Refs & Portals/src/components/Player.jsx
@@ -1,24 +1,19 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
+  const playerName = useRef();
   const [enteredpPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
 
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
-
-  function handleChange() {
-    setSubmitted(true);
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredpPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredpPlayerName ?? enteredpPlayerName}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredpPlayerName} />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
