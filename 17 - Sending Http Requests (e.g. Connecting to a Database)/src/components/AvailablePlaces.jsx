11f7f14e79ad5cc85d24998f4f8a7f67e0711675
hisham rehab <hishamrehab309@gmail.com>
2026-05-30 21:45:35 +0300

    fetching available places and the user location

--- 17 - Sending Http Requests (e.g. Connecting to a Database)/src/components/AvailablePlaces.jsx
+++ 17 - Sending Http Requests (e.g. Connecting to a Database)/src/components/AvailablePlaces.jsx
@@ -1,12 +1,56 @@
+import {useState , useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from "./Error.jsx"
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from "../http.js"
+const places =localStorage.getItem('places')
 
-export default function AvailablePlaces({ onSelectPlace }) {
+export default  function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState(places);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error , setError] = useState();
+
+  useEffect(() =>{
+    setIsFetching(true); 
+    async function fecthPlaces() {
+      
+      try {
+     const places =  await fetchAvailablePlaces();
+ 
+         navigator.geolocation.getCurrentPosition(
+            (position) =>{
+              const sortedPlaces =  sortPlacesByDistance(
+                places,
+                position.coords.latitude ,
+                  position.coords.longitude
+                )
+              setAvailablePlaces(sortedPlaces);
+              setIsFetching(false);
+            });
+        } catch (error){
+        setError({message: error.message || 'Could not fetch places , please try again later'});
+        }
+        setIsFetching(false);
+    }
+  fecthPlaces(false);
+  },  [])
+
+  if(error) {
+return <Error 
+     title="An error occured!" 
+     message={error.message}
+/>
+  }
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching ``}
+      loadinText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
   );
 }
+ 
