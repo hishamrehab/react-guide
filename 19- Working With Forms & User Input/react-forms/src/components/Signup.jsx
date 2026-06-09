8923ad70ae747b8d1ad7e27ab5b8561b26576d8d
hisham rehab <hishamrehab309@gmail.com>
2026-06-09 18:46:20 +0300

    Built-in validation logic

--- 19- Working With Forms & User Input/react-forms/src/components/Signup.jsx
+++ 19- Working With Forms & User Input/react-forms/src/components/Signup.jsx
@@ -15,19 +15,19 @@ export default function Signup() {
   }
 
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Welcome on board!</h2>
         <p>We just need a little bit of data from you to get you started 🚀</p>
   
         <div className="control">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email"  required />
         </div>
   
         <div className="control-row">
           <div className="control">
             <label htmlFor="password">Password</label>
-            <input id="password" type="password" name="password" />
+            <input id="password" type="password" name="password"  required />
           </div>
   
           <div className="control">
@@ -36,6 +36,9 @@ export default function Signup() {
               id="confirm-password"
               type="password"
               name="confirm-password"
+              required 
+              minLength={6}
+              maxLength={20}
             />
           </div>
         </div>
@@ -56,7 +59,7 @@ export default function Signup() {
   
         <div className="control">
           <label htmlFor="phone">What best describes your role?</label>
-          <select id="role" name="role">
+          <select id="role" name="role" required>
             <option value="student">Student</option>
             <option value="teacher">Teacher</option>
             <option value="employee">Employee</option>
@@ -88,14 +91,14 @@ export default function Signup() {
           </div>
   
           <div className="control">
-            <input type="checkbox" id="other" name="acquisition" value="other" />
+            <input type="checkbox" id="other" name="acquisition" value="other" required />
             <label htmlFor="other">Other</label>
           </div>
         </fieldset>
   
         <div className="control">
           <label htmlFor="terms-and-conditions">
-            <input type="checkbox" id="terms-and-conditions" name="terms" />I
+            <input type="checkbox" id="terms-and-conditions" name="terms"  required />I
             agree to the terms and conditions
           </label>
         </div>
