0f3014360175360856eb7c2f437ec6d3a1203aad
hisham rehab <hishamrehab309@gmail.com>
2026-06-23 23:01:35 +0300

    Header Implementation

---
+++ 21 - Building a Food Order  App/food-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <img />
+            <h1></h1>
+        </div>
+        <nav>
+            <button>Cart (0)</button>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
