a313b5ca8005c1e5e774c40382029a7ff272b330
hisham rehab <hishamrehab309@gmail.com>
2026-06-25 04:12:36 +0300

    Fetching & Using the Caty Context & Reducer

--- 21 - Building a Food Order  App/food-app/src/components/Header.jsx
+++ 21 - Building a Food Order  App/food-app/src/components/Header.jsx
@@ -1,7 +1,13 @@
 import Button from './UI/Button.jsx';
 import logoImg from '../assets/logo.jpg'
+import { useContext } from 'react';
+import CartContext from '../store/CartContext.jsx';
+
 
 const Header = () => {
+  const cartCxt = useContext(CartContext);
+  const totalCartItems = cartCxt.items.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <header id="main-header">
         <div id="title">
@@ -9,10 +15,10 @@ const Header = () => {
             <h1>ReactFoodOrder</h1>
         </div>
         <nav>
-            <Button textOnly>Cart (0)</Button>
+            <Button textOnly>Cart ({totalCartItems})</Button>
         </nav>
     </header>
   )
 }
-
+ 
 export default Header
